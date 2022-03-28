4b50a69a4e8b2734c0e002fc950b6a2815a40b4e
hakuryuu112 <rifki.ramdani@students.amikom.ac.id>
2022-03-28 20:43:47 +0700

    Third Commit laravelToko

--- app/Models/Customers.php
+++ app/Models/Customers.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Customers extends Model
 {
     use HasFactory;
+
+    protected $table = 'customers';
+    const CREATED_AT = null;
+    const UPDATE_AT = null;
+    protected $guarded = [];
 }
---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use app\Models\Customers;
+use Faker\Factory as DataPalsu;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dataPalsu = DataPalsu::create('id_ID');
+        $data = [];
+        for ($i = 0;  $i < 100; $i++)
+        {
+            $gender = $dataPalsu->randomElement(['male', 'female']);
+            $data[] = [
+                'email' => $dataPalsu->email(),
+                'first_name' => $dataPalsu->firstName($gender),
+                'last_name' => $dataPalsu->lastName(),
+                'city' => $dataPalsu->city(),
+                'address' => $dataPalsu->address(),
+                'password' => Hash::make('1234567')
+            ];
+        }
+        (new Customers())->insert($data);
+    }
+}
